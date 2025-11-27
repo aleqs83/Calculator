3d6e99a073ea536199528c22eab02b679aa80388
aleqs83 <aleqsavoyan@gmail.com>
2025-11-27 14:46:03 +0200

    claculator style

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,16 +2,29 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            LblDisplay.Text = "";
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void ClearAllButton(object sender, EventArgs e)
         {
-            
+            Button btn = (Button)sender;
+            LblDisplay.Text += btn;
+            if (btn.Text == "AC")
+            {
+                LblDisplay.Text = "";
+
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            LblDisplay.Text += btn.Text;
+
         }
     }
 
