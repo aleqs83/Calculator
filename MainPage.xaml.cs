41084dc3b3d0a6fbf5c5e1cf0ab427db92df0494
aleqs83 <aleqsavoyan@gmail.com>
2025-12-04 16:09:51 +0200

    arithmatic

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,7 +2,10 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        string arithmatic = "";
+        int num1 = 0;
+        int num2 = 0;
+        int total = 0;
         public MainPage()
         {
             InitializeComponent();
@@ -16,17 +19,54 @@
             if (btn.Text == "AC")
             {
                 LblDisplay.Text = "";
+                num1 = 0;
+                num2 = 0;
 
             }
         }
-
+        private void Button_eq_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (arithmatic == "+")
+            {
+              total = num1 + num2;
+            }
+            else if (arithmatic == "-")
+            {
+                total = num1 - num2;
+            }
+            else if (arithmatic == "*")
+            {
+                total = num1 * num2;
+            }
+            else if (arithmatic == "/")
+            {
+                total = num1 / num2;
+            }
+            LblDisplay.Text = "= " + total.ToString();
+                LblDisplay.Text = "";
+                num1 = 0;
+                num2 = 0;
+                arithmatic = "";
+                total = 0;         
+        }
+        private void ArithmeticButton_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            LblDisplay.Text += btn.Text;
+            arithmatic += btn.Text;
+        }
         private void Button_Clicked(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
             LblDisplay.Text += btn.Text;
-            if(btn.Text == "=")
+            if(arithmatic == "")
             {
-
+                num1 = num1 * 10 + int.Parse(btn.Text);
+            }
+            else
+            {
+                num2 = num2 * 10 + int.Parse(btn.Text);
             }
 
         }
